d3652a4936fe1c7c254a8572342385aae0108511
Oxana Theis <otheis@protonmail.com>
2022-12-13 12:52:51 +0100

    change contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Leafs from '../Leafs/Leafs'
 import './contact.css'
-
+import green from '../../assets/nature.png'
+//   <img className='test'src={green}alt=""/>
 function Contact() {
   return (
     <div className="contact" id="contact">
+     
+
+     <div class="waves">
+      <div class="wave" id="wave1"></div>
+      <div class="wave" id="wave2"></div>
+      <div class="wave" id="wave3"></div>
+      <div class="wave" id="wave4"></div>
+    </div>
+
     <h2>Got a project in mind ?</h2>
     <p>
       I'm <span style={{ color: "rgb(255, 4, 255)" }}>currently available</span> and looking to collaborate on new project.
@@ -13,7 +23,7 @@ function Contact() {
     </p>
     <button type="button">GET IN TOUCH</button>
 
-    <Leafs />
+  {/*  <Leafs />*/}
   </div>
   )
 }
