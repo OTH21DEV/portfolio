e9ae209ed94b266b053f6f2eb80cd49f396891a2
Oxana Theis <otheis@protonmail.com>
2024-07-03 15:21:23 +0200

    add new cv

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -16,8 +16,16 @@ import "./projects.css";
 function Projects() {
   return (
     <div className="container" id="projects">
-         <Card
+           <Card
         number={first}
+        name={"LAVCRAFT"}
+        description={"FRONTEND REACT WEB APP - build a corporate web site"}
+        site={"https://lavcraft.com/"}
+        skills={"JS React Heroku"}
+        githubLink={"https://github.com/OTH21DEV/agency_pro.git"}
+      ></Card>
+         <Card
+        number={second}
         name={"SOCIAL NETWORK"}
         description={"Fullstack- build RESTful API for corporate social network"}
         site={"https://corporate-social-network-902adf387c2e.herokuapp.com/"}
@@ -34,7 +42,7 @@ function Projects() {
       ></Card> */}
 
       <Card
-        number={second}
+        number={three}
         name={"ARGENT BANK"}
         description={"FRONTEND REACT WEB APP - use an API for a bank user account"}
         site={"https://argent-bank-p13.herokuapp.com/"}
@@ -42,7 +50,7 @@ function Projects() {
         githubLink={"https://github.com/OTH21DEV/OxanaTheis_13_03082022.git"}
       ></Card>
       <Card
-        number={three}
+        number={four}
         name={"SPORT SEE"}
         description={"FRONTEND REACT WEB APP - build an analytics dashboard"}
         site={"https://sportsee-p12.herokuapp.com/"}
@@ -51,7 +59,7 @@ function Projects() {
       ></Card>
 
       <Card
-        number={four}
+        number={five}
         name={"WEALTH HEALTH"}
         description={"FRONTEND REACT WEB APP - pass a jQuery library to React"}
         site={"https://oth21dev.github.io/OxanaTheis_14_27082022/"}
@@ -60,7 +68,7 @@ function Projects() {
       ></Card>
 
       <Card
-        number={five}
+        number={six}
         name={"KASA"}
         description={"FRONTEND REACT WEB APP - develop an app with React and React Router"}
         site={"https://kasa-oxanatheis.netlify.app/"}
@@ -69,7 +77,7 @@ function Projects() {
       ></Card>
 
       <Card
-        number={six}
+        number={seven}
         name={"LES PETITS PLATS"}
         description={"FRONTEND WEB APP - develop a searching engine in JavaScript"}
         site={"https://oth21dev.github.io/OxanaTheis_7_02012022/"}
@@ -77,7 +85,7 @@ function Projects() {
         githubLink={"https://github.com/OTH21DEV/OxanaTheis_7_02012022.git"}
       ></Card>
       <Card
-        number={seven}
+        number={eight}
         name={"FISH EYE"}
         description={"FRONTEND WEB APP - create an accessible site"}
         site={"https://oth21dev.github.io/OxanaTheis_6_12102021/"}
@@ -86,7 +94,7 @@ function Projects() {
       ></Card>
 
       <Card
-        number={eight}
+        number={nine}
         name={"GAME ON"}
         description={"FRONTEND WEB APP - develop a landing page with JavaScript"}
         site={"https://oth21dev.github.io/GameOn-website-FR/"}
@@ -94,7 +102,7 @@ function Projects() {
         githubLink={"https://github.com/OTH21DEV/GameOn-website-FR.git"}
       ></Card>
       <Card
-        number={nine}
+        number={ten}
         name={"OH MY FOOD"}
         description={"FRONTEND WEB APP - animate a web page with CSS animations"}
         site={"https://oth21dev.github.io/OxanaTheis_3_07062021/"}
@@ -102,14 +110,14 @@ function Projects() {
         githubLink={"https://github.com/OTH21DEV/OxanaTheis_3_07062021.git"}
       ></Card>
 
-      <Card
+      {/* <Card
         number={ten}
         name={"RESERVIA"}
         description={"FRONTEND WEB APP - turn a mockup into a website"}
         site={"https://oth21dev.github.io/OxanaTheis_2_05042020/"}
         skills={"HTML CSS"}
         githubLink={"https://github.com/OTH21DEV/OxanaTheis_2_05042020.git"}
-      ></Card>
+      ></Card> */}
 
       <FloatingTitle />
     </div>
